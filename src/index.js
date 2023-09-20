38beac8dfbab4a3aba6eef93c26b5299983fe507
Shashwat Sharma <shashwat.ashutosh.sharma@gmail.com>
2023-09-20 12:59:47 +0530

    Updated the js file with the API code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-var input_code = document.getElementById("input-code");
+import {default as axios} from "axios"
+import {stringify} from "qs"
+
+var input = document.getElementsByClassName("input")[0];
 var output = document.getElementsByClassName("output")[0];
 var expected = document.getElementsByClassName("expected")[0];
 var result = document.getElementsByClassName("result")[0];
 
-var run_code = function () {
-    output.value = get_result();
-    if(output.value.trim() === expected.value.trim())
-        result.value="Success";
-    else
-        result.value="Failure";
-};
+window.run_code = async function() {
+    output.value = "waiting";
 
-//TO-DO: Implement the API
-var get_result = function () {
-    return "Hello, World!"
+    var input_code = document.getElementById("input-code").value;
+
+    var data = stringify({
+        'code': input_code,
+        'language': 'c',
+        'input': input.value
+    });
+    
+    var config = {
+        method: 'post',
+        url: 'https://api.codex.jaagrav.in',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data : data
+    };
+
+    axios(config)
+        .then(function (response) {
+            var resp=JSON.stringify(response.data);
+            if(JSON.parse(resp).output === "")
+                output.value = JSON.parse(resp).error
+            else
+                output.value = JSON.parse(resp).output;
+            if(output.value.trim() === expected.value.trim())
+                result.value = "Success";
+            else
+                result.value = "Failure";
+        })
+        .catch(function (error) {
+            console.log(error);
+            output.value = "Unknown error";
+        });
 };
